01ce8a794cb1ee2b8f7c2b62e0e2870ab5e084a1
salma-mohammadieh <salma.mohammadieh@optimumpartners.co>
2024-03-19 09:35:13 +0300

    initail commit

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js'],
+        paths: ['src'],
+      },
+    },
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'plugin:cypress/recommended',
+    'plugin:json/recommended',
+  ],
+  plugins: ['import', 'prettier', 'react'],
+  root: true,
+  rules: {
+    'class-methods-use-this': 0,
+    'import/no-absolute-path': 2,
+    'import/no-extraneous-dependencies': 0,
+    'import/no-named-as-default': 1,
+    'import/no-unresolved': 2,
+    'eol-last': 2,
+    'no-multiple-empty-lines': 2,
+    'no-param-reassign': 0,
+    'no-throw-literal': 0,
+    'no-trailing-spaces': 2,
+    'no-unused-vars': [
+      'error',
+      { vars: 'all', args: 'none', ignoreRestSiblings: true },
+    ],
+    'prettier/prettier': 'error',
+    'no-unused-expressions': [2, { allowTaggedTemplates: true }],
+    'no-underscore-dangle': ['error', { allowAfterThis: true }],
+    'react/prop-types': 'error',
+    'react/require-default-props': 'error',
+  },
+  parser: '@babel/eslint-parser',
+  overrides: [
+    {
+      files: ['**/*.ejs'],
+      rules: {
+        'max-len': ['error', { code: 130 }],
+      },
+    },
+  ],
+};
