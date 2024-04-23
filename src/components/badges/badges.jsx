e49bfcb2e72ceb4fe13de3061a854aa55a5e0e1c
Dawood <dawood.abuswai@optimumpartners.co>
2024-04-23 13:20:44 +0300

    adding description to the badges and progressbar component

--- src/components/badges/badges.jsx
+++ src/components/badges/badges.jsx
@@ -28,9 +28,21 @@ Badges.defaultProps = {
 };
 
 Badges.propTypes = {
+  /**
+   * passed className to Badge's container
+   */
   className: PropTypes.string,
+  /**
+   * size of the Badges
+   */
   size: PropTypes.oneOf(['small', 'large']),
+  /**
+   * the number that is displayed in the badge
+   */
   label: PropTypes.number,
+  /**
+   * the maximum number you want to display of the label if the label is large
+   */
   maxNumber: PropTypes.number,
 };
 
